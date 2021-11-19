9ede9485550d02e0cdeb172fa124aaa5f4002ea6
Tareq23 <mdtarequlislam634@gmail.com>
2021-11-19 09:41:09 +0600

    Scaffold Widget appBar & body attributes

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,34 @@
 import 'package:flutter/material.dart';
-//
-//
-// void main()
-// {
-//   runApp(MaterialApp(
-//       home: Center(
-//         child:Text("Md Tarequl Islam")
-//       )
-//   ));
-// }
 
 
-void main(){
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-      home:Scaffold(
-        appBar:AppBar(
-          backgroundColor: Colors.lightBlueAccent,
-          title : Text("First Flutter App"),
-          centerTitle: true,
-        ),
-            body: Center(
-              child:Image(
-                image:NetworkImage('https://cdn.pixabay.com/photo/2021/11/12/18/13/greenfinch-6789772_960_720.jpg')
-              )
 
-      )
-      )
 
-  ));
+
+void main()
+{
+  runApp(HomeApp());
 }
+
+class HomeApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home:Scaffold(
+          backgroundColor: Colors.lightBlueAccent,
+          appBar: AppBar(
+            backgroundColor: Colors.cyan,
+            title: Text("Scaffold Widget"),
+            centerTitle: true,
+          ),
+          body: Center(
+
+            child: Text("Scaffold body attributes",style: TextStyle(color: Colors.black87,fontSize: 28)),
+
+          ),
+        )
+    );
+  }
+}
+
