2d529d867fb207975f6d66668923412b1ca585b9
Tareq23 <mdtarequlislam634@gmail.com>
2021-11-24 18:01:00 +0600

    remove unnecessary space

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
-
-
 void main(){
   runApp(WorkWithImage());
-
 }
-
 class WorkWithImage extends StatelessWidget
 {
   const WorkWithImage({Key? key}) : super(key: key);
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
