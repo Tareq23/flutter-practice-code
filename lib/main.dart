b54fdd587b3f6f55a907987b3207440c75efeb87
Tareq23 <mdtarequlislam634@gmail.com>
2021-11-24 12:53:13 +0600

    works with image, assets, pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
-  runApp(ScaffoldWidgetPractice());
+
+void main(){
+  runApp(WorkWithImage());
+
 }
 
-class ScaffoldWidgetPractice extends StatelessWidget
+class WorkWithImage extends StatelessWidget
 {
+  const WorkWithImage({Key? key}) : super(key: key);
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: _StateFulWidget()
-    );
-  }
 
-}
+        debugShowCheckedModeBanner: false,
+        home:Scaffold(
 
-class _StateFulWidget extends StatefulWidget{
-  @override
-  MyStatefulWidget createState() => MyStatefulWidget();
-}
-
-class MyStatefulWidget extends State<_StateFulWidget>
-{
-  int count=0;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter Scaffold Widget"),
-          centerTitle: true,
+          appBar: AppBar(
+            title: Text("Developed by MD Tarequl Islam"),
+            centerTitle: true,
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: <Widget> [
+                Image.asset("assets/box.jpg"),
+                Divider(height: 1.0,),
+                Image.asset("assets/facebook.jpg"),
+                Divider(height: 1.0,),
+                Image.asset("assets/laptop.jpg"),
+                Divider(height: 1.0,),
+                Image.asset("assets/monitor.jpg"),
+              ],
+            ),
+          )
         ),
-      body: Center(
-        child:Text("The have pressed the ___count times",style:TextStyle(color:Colors.greenAccent,fontSize: 28.0)),
-
-      ),
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        child: Container(
-          height: 50.0,
-        )
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() {
-          count++;
-          print(count);
-        }),
-        tooltip: 'Increment Counter',
-        child: Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.startDocked,
-      drawer: Drawer(
-        elevation: 8.0,
-        child: Column(
-          children: [
-            UserAccountsDrawerHeader(
-              accountEmail: Text('tareq@gmail.com'),
-              accountName: Text('tareq23'),
-              currentAccountPicture: CircleAvatar(
-                backgroundColor: Colors.yellow,
-                child: Text('abc')
-              ),
-            ),
-            ListTile(
-              title: Text("Inbox"),
-              leading: Icon(Icons.mail),
-            ),
-            Divider(
-              height: 2.0,
-            ),
-            ListTile(
-                title: Text("People"),
-              leading: Icon(Icons.people),
-            ),
-            ListTile(
-              title: Text("Promotion"),
-              leading: Icon(Icons.local_offer),
-            ),
-            ListTile(
-              title: Text("Primary"),
-              leading: Icon(Icons.people),
-            )
-          ],
-        )
-      ),
     );
   }
 
+
 }
-
-
-
-
-
-
