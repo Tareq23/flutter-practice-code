7e75464f78d8cd2942ad88614034eae68c859aa6
Tareq23 <mdtarequlislam634@gmail.com>
2021-11-28 18:18:51 +0600

    Form create and style a text field

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,96 @@
+
+
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(){
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget
+{
   @override
-  Widget build(BuildContext constext) {
-    return const MaterialApp(
-      home: HomeState(),
+  Widget build(BuildContext context){
+    const appTitle = "Form Demo";
+    return  MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: appTitle,
+        home: HomeState(),
     );
   }
 }
 
-class HomeState extends StatefulWidget {
-  const HomeState({Key? key}) : super(key: key);
-
+class HomeState extends StatefulWidget
+{
   @override
-  MyCustomFormWidget createState() {
-    return MyCustomFormWidget();
+  CustomForm createState (){
+    return CustomForm();
   }
 }
 
-class MyCustomFormWidget extends State<HomeState> {
+class CustomForm  extends State<HomeState>{
   @override
   Widget build(BuildContext context) {
-    final _formKey = GlobalKey<FormState>();
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "My custom form widget",
-          style: TextStyle(color: Colors.grey),
+     return  Scaffold(
+        appBar: AppBar(
+          title: const Text("Form Create Style", style: TextStyle(fontWeight: FontWeight.w900),),
         ),
-        centerTitle: true,
-      ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  // ignore: avoid_print
-                  print("Please Enter Some text");
-                  return "Please Enter some text";
-                } else {
-                  // ignore: avoid_print
-                  print(value);
-                }
-              },
+       body: Column(
+         children:   <Widget>[
+           const Padding(
+               padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+             child: TextField(
+               decoration: InputDecoration(
+                 enabledBorder: OutlineInputBorder(),
+                 hintText: 'Enter a search term'
+               ),
+             ),
+           ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 80, vertical: 16),
+            child:  TextField(
+              decoration:  InputDecoration(
+                border: UnderlineInputBorder(),
+                hintText: 'Enter your username',
+              ),
             ),
-            ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                      content: Text("processing Data"),
-                    ));
-                  }
-                  // _formKey.currentState!.validate()
-                },
-                child: const Text(
-                  "Submit",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                )),
-          ],
-        ),
-      ),
-    );
+          ),
+           Padding(
+             padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 20),
+             child:  TextFormField(
+               decoration:  InputDecoration(
+                 labelText: 'Enter something',
+                 enabledBorder: OutlineInputBorder(
+                   borderSide: const BorderSide(width: 3, color: Colors.greenAccent),
+                   borderRadius:  BorderRadius.circular(20),
+                 ),
+                 focusedBorder:  OutlineInputBorder(
+                   // borderSide: BorderSide(width: 20, color: Colors.redAccent),
+                   borderSide: const BorderSide(width: 5, color: Colors.cyanAccent),
+                   // borderRadius:  BorderRadius.zero,
+                   borderRadius: BorderRadius.circular(15),
+                 )
+
+               ),
+             ),
+           ),
+           Padding(
+             padding: const EdgeInsets.fromLTRB(20, 50, 20, 10),
+             child: TextFormField(
+               decoration:  InputDecoration(
+                 hintText: 'Write something',
+                 labelText: 'Always write something',
+                 border: OutlineInputBorder(
+                   borderSide: const BorderSide(width: 3, color: Colors.pink),
+                   borderRadius: BorderRadius.circular(16),
+                 )
+               ),
+             ),
+           )
+         ],
+       )
+     );
   }
 }
+
