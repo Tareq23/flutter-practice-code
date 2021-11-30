eacebcf62e6ebc53ed0eca3121edbac0ef657d0a
Tareq23 <mdtarequlislam634@gmail.com>
2021-11-30 15:55:11 +0600

    container, Row, Column, SafeArea, SingleChildScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,8 @@ class MyApp extends StatelessWidget{
 
 class CustomStatefulWidget extends StatefulWidget
 {
+  const CustomStatefulWidget({Key? key}) : super(key: key);
+
   @override
   _MyState createState () => _MyState();
 }
@@ -30,106 +32,109 @@ class _MyState extends State<CustomStatefulWidget>
       // appBar: AppBar(),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children:  [
-              const Text("Hello world"),
-              Container(
-                width: 200,
-                height: 200,
-                color: Colors.green,
-                child: const Text("Container Widget", style: TextStyle(
-                  fontSize: 30,
-                ),),
-              ),
-              Container(
-                width: 200,
-                height: 200,
-                color: Colors.red,
-                child: const Text("Container Widget", style: TextStyle(
-                  fontSize: 30,
-                ),),
-              ),
-              Container(
-                width: 200,
-                height: 200,
-                color: Colors.blue,
-                child: const Text("Container Widget", style: TextStyle(
-                  fontSize: 30,
-                ),),
-              ),
-              Container(
-                width: 200,
-                height: 200,
-                color: Colors.blue,
-                child: const Text("Container Widget", style: TextStyle(
-                  fontSize: 30,
-                ),),
-              ),
-              Container(
-                width: 200,
-                height: 200,
-                color: Colors.blue,
-                child: const Text("Container Widget", style: TextStyle(
-                  fontSize: 30,
-                ),),
-              ),
-              Container(
-                width: 200,
-                height: 200,
-                color: Colors.blue,
-                child: const Text("Container Widget", style: TextStyle(
-                  fontSize: 30,
-                ),),
-              ),
-              Container(
-                width: 200,
-                height: 200,
-                color: Colors.blue,
-                child: const Text("Container Widget", style: TextStyle(
-                  fontSize: 30,
-                ),),
-              ),
-              Container(
-                width: 200,
-                height: 200,
-                color: Colors.blue,
-                child: const Text("Container Widget", style: TextStyle(
-                  fontSize: 30,
-                ),),
-              ),
-              Container(
-                width: 200,
-                height: 200,
-                color: Colors.blue,
-                child: const Text("Container Widget", style: TextStyle(
-                  fontSize: 30,
-                ),),
-              ),
-              Container(
-                width: 200,
-                height: 200,
-                color: Colors.blue,
-                child: const Text("Container Widget", style: TextStyle(
-                  fontSize: 30,
-                ),),
-              ),
-              Container(
-                width: 200,
-                height: 200,
-                color: Colors.blue,
-                child: const Text("Container Widget", style: TextStyle(
-                  fontSize: 30,
-                ),),
-              ),
-              Container(
-                width: 200,
-                height: 200,
-                color: Colors.blue,
-                child: const Text("Container Widget", style: TextStyle(
-                  fontSize: 30,
-                ),),
-              ),
-            ],
+          child: Center(
+            child: Column(
+              children:  [
+                const Text("Hello world"),
+                Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.green,
+                  child: const Text("Container Widget", style: TextStyle(
+                    fontSize: 30,
+                  ),),
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.red,
+                  child: const Text("Container Widget", style: TextStyle(
+                    fontSize: 30,
+                  ),),
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  // margin: EdgeInsets.all(20),
+                  color: Colors.blue,
+                  child: const Text("Container Widget", style: TextStyle(
+                    fontSize: 30,
+                  ),),
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.deepPurple,
+                  child: const Text("Container Widget", style: TextStyle(
+                    fontSize: 30,
+                  ),),
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.blue,
+                  child: const Text("Container Widget", style: TextStyle(
+                    fontSize: 30,
+                  ),),
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.yellow,
+                  child: const Text("Container Widget", style: TextStyle(
+                    fontSize: 30,
+                  ),),
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.blue,
+                  child: const Text("Container Widget", style: TextStyle(
+                    fontSize: 30,
+                  ),),
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.pink,
+                  child: const Text("Container Widget", style: TextStyle(
+                    fontSize: 30,
+                  ),),
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.blue,
+                  child: const Text("Container Widget", style: TextStyle(
+                    fontSize: 30,
+                  ),),
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.yellow,
+                  child: const Text("Container Widget", style: TextStyle(
+                    fontSize: 30,
+                  ),),
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.blue,
+                  child: const Text("Container Widget", style: TextStyle(
+                    fontSize: 30,
+                  ),),
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.black,
+                  child: const Text("Container Widget", style: TextStyle(
+                    fontSize: 30,
+                  ),),
+                ),
+              ],
+            ),
           ),
         )
       )
