65ad7cb35bc3323474aed04fc9ec882a75a5541b
Tareq23 <mdtarequlislam634@gmail.com>
2021-12-02 15:31:56 +0600

    Drawer widget for app Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,77 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-        home: CustomStatefulWidget(),
+      debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar:AppBar(
+            title: const Text("Drawer Widget"),
+
+          ),
+
+          drawer:  Drawer(
+
+            child: ListView(
+              padding: const EdgeInsets.only(top:20,),
+              children: const [
+                UserAccountsDrawerHeader(
+                    accountName:  Text("User Account"),
+                    accountEmail:  Text("example@checker.com"),
+                    currentAccountPicture: CircleAvatar(
+                      backgroundImage: NetworkImage("https://cdn.pixabay.com/photo/2021/09/13/05/34/kid-6620283_960_720.jpg"),
+                    ),
+                ),
+                ListTile(
+                  title: Text("Contact"),
+                  // trailing: Icon(Icons.contact_mail),
+                  trailing: Icon(Icons.arrow_forward),
+                ),
+                ListTile(
+                  title: Text("Portfolio"),
+                  // trailing: Icon(Icons.contact_mail),
+                  trailing: Icon(Icons.arrow_forward),
+                ),ListTile(
+                  title: Text("Social Info"),
+                  // trailing: Icon(Icons.contact_mail),
+                  trailing: Icon(Icons.arrow_forward),
+                ),ListTile(
+                  title: Text("Team"),
+                  // trailing: Icon(Icons.contact_mail),
+                  trailing: Icon(Icons.arrow_forward),
+                ),ListTile(
+                  title: Text("Address"),
+                  // trailing: Icon(Icons.contact_mail),
+                  trailing: Icon(Icons.arrow_forward),
+                ),ListTile(
+                  title: Text("Education Info"),
+                  // trailing: Icon(Icons.contact_mail),
+                  trailing: Icon(Icons.arrow_forward),
+                ),ListTile(
+                  title: Text("Logout"),
+                  // trailing: Icon(Icons.contact_mail),
+                  trailing: Icon(Icons.arrow_forward),
+                ),
+              ],
+            ),
+          ),
+        ),
+
     );
   }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class CustomStatefulWidget extends StatefulWidget
 {
