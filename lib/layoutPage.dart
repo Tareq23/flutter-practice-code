bc492c85087816dc0202b8c0ec754cac5a87306d
Tareq23 <mdtarequlislam634@gmail.com>
2021-12-26 14:45:54 +0600

    previous ui updated

--- lib/layoutPage.dart
+++ lib/layoutPage.dart
@@ -31,6 +31,164 @@ class _HomeWidgetState extends State<HomeWidget> {
               fit: BoxFit.cover,
             ),
             Positioned(
+              top: 150,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 300,
+                decoration: BoxDecoration(
+                  // color: Colors.white.withOpacity(0.4)
+                  color: Colors.transparent,
+                ),
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                 children: [
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      ),
+                      child: Stack(
+                        children: [
+                          Image(
+                            width: 250,
+                            height: double.infinity,
+                            image: NetworkImage(
+                              "https://cdn.pixabay.com/photo/2021/11/09/15/32/christmas-6781762_960_720.jpg",
+                            ),
+                            fit: BoxFit.cover,
+                          ),
+                          Positioned(
+                            left: 5,
+                            right: 5,
+                            top: 200,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('Place Name',style: TextStyle(fontSize: 16,fontWeight: FontWeight.w700,color: Colors.deepOrange),),
+                                ActionChip(
+                                    onPressed:(){},
+                                    label: Text('Show Details', style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500,color: Colors.deepOrange),),
+                                    backgroundColor: Colors.lightGreenAccent,
+                                ),
+
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      ),
+                      child: Stack(
+                        children: [
+                          Image(
+                            width: 250,
+                            height: double.infinity,
+                            image: NetworkImage(
+                              "https://cdn.pixabay.com/photo/2016/11/29/03/44/animal-1867125_960_720.jpg",
+                            ),
+                            fit: BoxFit.cover,
+                          ),
+                          Positioned(
+                            left: 5,
+                            right: 5,
+                            top: 200,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('Place Name',style: TextStyle(fontSize: 16,fontWeight: FontWeight.w700,color: Colors.deepOrange),),
+                                ActionChip(
+                                  onPressed:(){},
+                                  label: Text('Show Details', style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500,color: Colors.deepOrange),),
+                                  backgroundColor: Colors.lightGreenAccent,
+                                ),
+
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      ),
+                      child: Stack(
+                        children: [
+                          Image(
+                            width: 250,
+                            height: double.infinity,
+                            image: NetworkImage(
+                              "https://cdn.pixabay.com/photo/2019/10/30/10/33/girl-4589231_960_720.jpg",
+                            ),
+                            fit: BoxFit.cover,
+                          ),
+                          Positioned(
+                            left: 5,
+                            right: 5,
+                            top: 200,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('Place Name',style: TextStyle(fontSize: 16,fontWeight: FontWeight.w700,color: Colors.deepOrange),),
+                                ActionChip(
+                                  onPressed:(){},
+                                  label: Text('Show Details', style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500,color: Colors.deepOrange),),
+                                  backgroundColor: Colors.lightGreenAccent,
+                                ),
+
+                              ],
+                            ),
+                          ),
+
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      ),
+                      child: Stack(
+                        children: [
+                          Image(
+                            width: 250,
+                            height: double.infinity,
+                            image: NetworkImage(
+                              "https://cdn.pixabay.com/photo/2021/02/04/13/28/bird-5981360_960_720.jpg",
+                            ),
+                            fit: BoxFit.cover,
+                          ),
+                          Positioned(
+                            left: 5,
+                            right: 5,
+                            top: 200,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('Place Name',style: TextStyle(fontSize: 16,fontWeight: FontWeight.w700,color: Colors.deepOrange),),
+                                ActionChip(
+                                  onPressed:(){},
+                                  label: Text('Show Details', style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500,color: Colors.deepOrange),),
+                                  backgroundColor: Colors.lightGreenAccent,
+                                ),
+
+                              ],
+                            ),
+                          ),
+
+                        ],
+                      ),
+                    ),
+                 ],
+                ),
+              ),
+            ),
+            Positioned(
               left: 10,
               bottom: 10,
               right: 10,
