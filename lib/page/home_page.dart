3c5a3895e00884fc14740913cae34438badf3c38
Tareq23 <mdtarequlislam634@gmail.com>
2022-01-01 19:49:44 +0600

    Use ElevatedButton.icon constructor

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -26,6 +27,34 @@ class _HomeStateState extends State<HomeState> {
       appBar: AppBar(
         backgroundColor: Colors.purple[300],
         title: const Text('Google Auth',style: TextStyle(fontSize: 22,fontWeight: FontWeight.w700,color: Colors.white70),),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 150,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                margin: const EdgeInsets.all(0),
+                padding: const  EdgeInsets.symmetric(vertical: 10,horizontal: 20),
+                child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.deepOrangeAccent,
+                  ),
+
+                  onPressed: (){},
+                  icon: const FaIcon(FontAwesomeIcons.google),
+                  label: const Text('Google Sign In',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 22,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
