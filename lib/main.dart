31d7cf36a3f4d5b263e94c0bc42d2337e990638f
Tareq23 <mdtarequlislam634@gmail.com>
2022-01-01 19:33:29 +0600

    ready for google sign in

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,11 @@
 import 'package:demoapp/page/home_page.dart';
 import 'package:flutter/cupertino.dart';
 
-void main()
+import 'package:firebase_core/firebase_core.dart';
+
+Future main() async
 {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(HomePage());
 }
